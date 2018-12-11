087736e504b836bfa31019a9acf22bbf7a0da2ff
mitchthebaker <mitchrpbaker@gmail.com>
2018-12-11 05:22:41 -0800

    Set up server file app.js, added .get('/', ...) for default index.ejs page

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require('express');
 const PORT = process.env.PORT || 5002;
 const app = express();
 
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/scripts')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index.ejs'));
 app.listen(PORT);
