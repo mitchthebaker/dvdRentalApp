9eccc5d7e7eb20dc65ed0718e0b4651637f1f7ae
mitchthebaker <mitchrpbaker@gmail.com>
2019-01-07 11:33:57 -0800

    installed pg packages, setting up database to be used

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index.ejs'));
 app.listen(PORT);
+
+const { Client } = require('pg');
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+
+client.connect();
