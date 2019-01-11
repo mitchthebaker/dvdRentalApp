0585356a55a151bc8fc8f99516a47997829e3f82
mitchthebaker <mitchrpbaker@gmail.com>
2019-01-11 04:52:41 -0800

    Created db.ejs file and made changes to server

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/scripts')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index.ejs'));
-app.listen(PORT);
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/db', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		const addQuery = await client.query("INSERT INTO card_list VALUES (2, 'aye', 'whats good')");
+		console.log(addQuery);
+		const result = await client.query('SELECT * FROM card_list');
+  		const results = {
+  			'results': (result) ? result.rows : null
+  		};
+
+  		res.render('pages/db', results);
+  		client.release();
+	} 
+	catch(err) {
+		console.error(err);
+  		res.send("Current error: " + err);
+	}
+});
+app.listen(PORT, () => console.log(`Currently listening on ${ PORT }`));
 
 const { Client } = require('pg');
 
